7872d982cc463e97d05076a3c8cfb714dc989815
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-09-02 02:40:31 +0530

    Updated

--- Routes/videoRoute.js
+++ Routes/videoRoute.js
@@ -4,9 +4,9 @@ const router = require('express').Router();
 
 router.get('/create-room', createRoom);
 router.get('/join', join);
-router.get('/manage', manageCall);
+router.post('/manage', manageCall);
 router.get('/notify', sendCallRequest);
-router.post('/call', getCallHistory);
+router.get('/call', getCallHistory);
 // router.get("/send-call-request",sendCallRequest);
 
 module.exports = router;
