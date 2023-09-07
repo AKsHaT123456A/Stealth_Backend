3db07e182a2c5274fe05dade902ca81aba7d3e25
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-09-08 02:10:45 +0530

    Auth

--- Controllers/profileController.js
+++ Controllers/profileController.js
@@ -6,7 +6,7 @@ const profile = async (req, res) => {
         const { id } = req.params;
         const { shopName } = req.body.shopName;
         const shopLink = `https://stealth-zys3.onrender.com/api/v1/video/join?roomName=${shopName}`;
-        await seller.findByIdAndUpdate({ _id: id }, { $set: { shopLink, ...req.body } });
+        await seller.findByIdAndUpdate({ _id: id }, { $set: { shopLink:shopLink, ...req.body } });
         return res.status(200).json({ message: "Profile Updated" });
     } catch (error) {
         return res.status(500).json({ message: "Something went wrong", error: error.message });
